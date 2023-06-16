ebeddb18ffd13f97d130c18b7280d32e76c4539e
raihana <122587829+rhnanor@users.noreply.github.com>
2023-06-16 20:31:27 +0800

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,43 +1,11 @@
 import streamlit as st
-import pandas as pd
-import altair as alt
+from streamlit import components
 
-# Dictionary of country names and their corresponding data links
-country_links = {
-    'Indonesia': 'https://tradingeconomics.com/indonesia/tourist-arrivals',
-    'Malaysia': 'https://tradingeconomics.com/malaysia/tourist-arrivals',
-    'Thailand': 'https://tradingeconomics.com/thailand/tourist-arrivals'
-    # Add more countries and their links here
-}
+st.title("Tourist Arrivals in Indonesia")
 
-# Function to load data from the given link
-def load_data(link):
-    data = pd.read_html(link)[0]
-    # Perform any necessary data preprocessing
-    return data
+# Embed the bar chart into the website using an iframe
+iframe_code = "<iframe src='https://tradingeconomics.com/embed/?s=indonesiatouarr&v=202306050459v20230410&h=300&w=600&ref=/indonesia/tourist-arrivals' height='300' width='600' frameborder='0' scrolling='no'></iframe>"
+components.html(iframe_code)
 
-# Function to create a bar chart using Altair
-def create_bar_chart(data):
-    chart = alt.Chart(data).mark_bar().encode(
-        x='Year',
-        y='Tourist Arrivals',
-        tooltip=['Year', 'Tourist Arrivals']
-    ).properties(
-        width=600,
-        height=400
-    )
-    return chart
-
-# Main app code
-st.title("Tourist Arrivals by Country")
-
-# Select the country
-selected_country = st.selectbox("Select a country", list(country_links.keys()))
-
-# Load data for the selected country
-data = load_data(country_links[selected_country])
-
-# Create and display the bar chart
-chart = create_bar_chart(data)
-st.altair_chart(chart, use_container_width=True)
-st.altair_chart(chart, use_container_width=True)
+# Provide the source link
+st.markdown("Source: [tradingeconomics.com](https://tradingeconomics.com/indonesia/tourist-arrivals)")
