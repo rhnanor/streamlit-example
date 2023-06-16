ddfe2a35ebdd6dc2d1a2c073d10dae1121d3fb3e
raihana <122587829+rhnanor@users.noreply.github.com>
2023-06-16 20:28:24 +0800

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,6 +1,6 @@
 import streamlit as st
 import pandas as pd
-import plotly.express as px
+import altair as alt
 
 # Load data from the website
 data_link = "https://tradingeconomics.com/indonesia/tourist-arrivals"
@@ -9,8 +9,14 @@ data = pd.read_html(data_link)[0]
 # Clean and preprocess the data if needed
 # (extract the necessary columns, handle missing values, etc.)
 
-# Create a bar chart using Plotly
-fig = px.bar(data, x='Year', y='Tourist Arrivals', labels={'Tourist Arrivals': 'Tourist Arrivals'}, title='Tourist Arrivals in Indonesia')
+# Create a bar chart using Altair
+chart = alt.Chart(data).mark_bar().encode(
+    x='Year',
+    y='Tourist Arrivals',
+    tooltip=['Year', 'Tourist Arrivals']
+).properties(
+    title='Tourist Arrivals in Indonesia'
+)
 
-# Display the bar chart in Streamlit
-st.plotly_chart(fig)
+# Display the chart in Streamlit
+st.altair_chart(chart, use_container_width=True)
