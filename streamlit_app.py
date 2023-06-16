81f16cdbc96937a0ece8b60877359edb0211bd8c
raihana <122587829+rhnanor@users.noreply.github.com>
2023-06-16 20:26:14 +0800

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,33 +1,25 @@
 import streamlit as st
 import pandas as pd
 
-# Define website links and corresponding images for travel data
-website_links = {
-    "Thailand": {
-        "link": "https://public.tableau.com/app/profile/tan.zhi.xuan/viz/TouristArrivalsForecatingDashboardBasedonCovid-19/Main",
-        "image": "thailand.png"
-    },
-    "Malaysia": {
-        "link": "https://public.tableau.com/app/profile/tan.zhi.xuan/viz/TouristArrivalsForecatingDashboardBasedonCovid-19/Main",
-        "image": "malaysia.png"
-    },
-    "Indonesia": {
-        "link": "https://public.tableau.com/app/profile/tan.zhi.xuan/viz/TouristArrivalsForecatingDashboardBasedonCovid-19/Main",
-        "image": "indonesia.png"
-    },
-    "Vietnam": {
-        "link": "https://public.tableau.com/app/profile/tan.zhi.xuan/viz/TouristArrivalsForecatingDashboardBasedonCovid-19/Main",
-        "image": "vietnam.png"
-    }
-}
+import streamlit as st
+import pandas as pd
+import matplotlib.pyplot as plt
 
-# Create a sidebar for user input
-st.sidebar.title("Travel Pattern Explorer")
-selected_country = st.sidebar.selectbox("Select a Country", list(website_links.keys()))
+# Load data from the website
+data_link = "https://tradingeconomics.com/indonesia/tourist-arrivals"
+data = pd.read_html(data_link)[0]
 
-# Display the selected country with the associated image and clickable link
-st.title(f"Travel Pattern in {selected_country}")
-image_path = website_links[selected_country]["image"]
-image = open(image_path, "rb")
-st.image(image, caption=f"{selected_country} Data Visualization")
-st.write(f"Click [here]({website_links[selected_country]['link']}) to access the travel information for {selected_country}.")
+# Clean and preprocess the data if needed
+# (extract the necessary columns, handle missing values, etc.)
+
+# Create a bar chart
+plt.figure(figsize=(10, 6))
+plt.bar(data['Year'], data['Tourist Arrivals'])
+plt.xlabel('Year')
+plt.ylabel('Tourist Arrivals')
+plt.title('Tourist Arrivals in Indonesia')
+plt.xticks(rotation=45)
+plt.tight_layout()
+
+# Display the bar chart in Streamlit
+st.pyplot(plt)
