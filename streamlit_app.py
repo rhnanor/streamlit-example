228faba786876a7282b92aa69edbbe9c2896a869
raihana <122587829+rhnanor@users.noreply.github.com>
2023-06-16 20:34:09 +0800

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,11 +1,10 @@
 import streamlit as st
-from streamlit import components
 
 st.title("Tourist Arrivals in Indonesia")
 
-# Embed the bar chart into the website using an iframe
-iframe_code = "<iframe src='https://tradingeconomics.com/embed/?s=indonesiatouarr&v=202306050459v20230410&h=300&w=600&ref=/indonesia/tourist-arrivals' height='300' width='600' frameborder='0' scrolling='no'></iframe>"
-components.html(iframe_code)
+# Embed the iframe code into the website
+iframe_code = "<iframe src='https://d3fy651gv2fhd3.cloudfront.net/embed/?s=indonesiatouarr&v=202306050459V20230410&h=300&w=600' height='300' width='600' frameborder='0' scrolling='no'></iframe>"
+st.markdown(iframe_code, unsafe_allow_html=True)
 
 # Provide the source link
 st.markdown("Source: [tradingeconomics.com](https://tradingeconomics.com/indonesia/tourist-arrivals)")
