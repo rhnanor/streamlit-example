0e593b3c0cb29f6d5c6bb4279724dc9c110799b9
raihana <122587829+rhnanor@users.noreply.github.com>
2023-06-16 20:30:16 +0800

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -2,21 +2,42 @@ import streamlit as st
 import pandas as pd
 import altair as alt
 
-# Load data from the website
-data_link = "https://tradingeconomics.com/indonesia/tourist-arrivals"
-data = pd.read_html(data_link)[0]
+# Dictionary of country names and their corresponding data links
+country_links = {
+    'Indonesia': 'https://tradingeconomics.com/indonesia/tourist-arrivals',
+    'Malaysia': 'https://tradingeconomics.com/malaysia/tourist-arrivals',
+    'Thailand': 'https://tradingeconomics.com/thailand/tourist-arrivals'
+    # Add more countries and their links here
+}
 
-# Clean and preprocess the data if needed
-# (extract the necessary columns, handle missing values, etc.)
+# Function to load data from the given link
+def load_data(link):
+    data = pd.read_html(link)[0]
+    # Perform any necessary data preprocessing
+    return data
 
-# Create a bar chart using Altair
-chart = alt.Chart(data).mark_bar().encode(
-    x='Year',
-    y='Tourist Arrivals',
-    tooltip=['Year', 'Tourist Arrivals']
-).properties(
-    title='Tourist Arrivals in Indonesia'
-)
+# Function to create a bar chart using Altair
+def create_bar_chart(data):
+    chart = alt.Chart(data).mark_bar().encode(
+        x='Year',
+        y='Tourist Arrivals',
+        tooltip=['Year', 'Tourist Arrivals']
+    ).properties(
+        width=600,
+        height=400
+    )
+    return chart
 
-# Display the chart in Streamlit
+# Main app code
+st.title("Tourist Arrivals by Country")
+
+# Select the country
+selected_country = st.selectbox("Select a country", list(country_links.keys()))
+
+# Load data for the selected country
+data = load_data(country_links[selected_country])
+
+# Create and display the bar chart
+chart = create_bar_chart(data)
+st.altair_chart(chart, use_container_width=True)
 st.altair_chart(chart, use_container_width=True)
