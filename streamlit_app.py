e35404617489ece8e5a8eb11780b34c5bc67c071
raihana <122587829+rhnanor@users.noreply.github.com>
2023-06-16 20:48:33 +0800

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,16 +1,27 @@
 import streamlit as st
 
-st.title("Tourist Arrivals in Indonesia")
-st.write("Welcome to the Tourist Arrivals Dashboard for Indonesia. This dashboard provides an interactive visualization of the tourist arrivals data for Indonesia. Explore the chart below to understand the trends and patterns in tourist arrivals over time. The data is sourced from tradingeconomics.com.")
+# Function to display the chart and description
+def display_chart(country_name, iframe_code):
+    st.title(f"Tourist Arrivals in {country_name}")
+    st.write(f"Welcome to the Tourist Arrivals Dashboard for {country_name}. This dashboard provides an interactive visualization of the tourist arrivals data for {country_name}. Explore the chart below to understand the trends and patterns in tourist arrivals over time. The data is sourced from tradingeconomics.com.")
+    st.write(iframe_code, unsafe_allow_html=True)
+    st.markdown(f"Source: [tradingeconomics.com](https://tradingeconomics.com/{country_name.lower().replace(' ', '-')}/tourist-arrivals)")
 
-
-# HTML code to embed the iframe
-iframe_code = """
-<iframe src='https://d3fy651gv2fhd3.cloudfront.net/embed/?s=indonesiatouarr&v=202306050459V20230410&h=300&w=600' height='300' width='600' frameborder='0' scrolling='no'></iframe>
+# Displaying chart for Malaysia
+malaysia_iframe = """
+<iframe src='https://d3fy651gv2fhd3.cloudfront.net/embed/?s=malaysiatouarr&v=202304061134V20230410&h=300&w=600' height='300' width='600' frameborder='0' scrolling='no'></iframe>
 """
+display_chart("Malaysia", malaysia_iframe)
 
-# Display the iframe using the HTML code
-st.write(iframe_code, unsafe_allow_html=True)
+# Displaying chart for Singapore
+singapore_iframe = """
+<iframe src='https://tradingeconomics.com/embed/?s=singaporetouarr&v=202306090122v20230410&h=300&w=600&ref=/singapore/tourist-arrivals' height='300' width='600' frameborder='0' scrolling='no'></iframe>
+"""
+display_chart("Singapore", singapore_iframe)
 
-# Provide the source link
-st.markdown("Source: [tradingeconomics.com](https://tradingeconomics.com/indonesia/tourist-arrivals)")
+# Displaying chart for Thailand
+thailand_iframe = """
+<iframe src='https://tradingeconomics.com/embed/?s=thailandtouarr&v=202305260335v20230410&h=300&w=600&ref=/thailand/tourist-arrivals' height='300' width='600' frameborder='0' scrolling='no'></iframe>
+"""
+display_chart("Thailand", thailand_iframe)
+
