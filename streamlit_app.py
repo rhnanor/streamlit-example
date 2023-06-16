705598a8a5af0ca02cc3c8f8a7a3abf1e2f5e7a5
raihana <122587829+rhnanor@users.noreply.github.com>
2023-06-16 20:35:42 +0800

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -2,9 +2,13 @@ import streamlit as st
 
 st.title("Tourist Arrivals in Indonesia")
 
-# Embed the iframe code into the website
-iframe_code = "<iframe src='https://d3fy651gv2fhd3.cloudfront.net/embed/?s=indonesiatouarr&v=202306050459V20230410&h=300&w=600' height='300' width='600' frameborder='0' scrolling='no'></iframe>"
-st.markdown(iframe_code, unsafe_allow_html=True)
+# HTML code to embed the iframe
+iframe_code = """
+<iframe src='https://d3fy651gv2fhd3.cloudfront.net/embed/?s=indonesiatouarr&v=202306050459V20230410&h=300&w=600' height='300' width='600' frameborder='0' scrolling='no'></iframe>
+"""
+
+# Display the iframe using the HTML code
+st.write(iframe_code, unsafe_allow_html=True)
 
 # Provide the source link
 st.markdown("Source: [tradingeconomics.com](https://tradingeconomics.com/indonesia/tourist-arrivals)")
