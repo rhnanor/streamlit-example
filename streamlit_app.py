aa0444f799a740cee5c8f1e252ccfcf86483d555
raihana <122587829+rhnanor@users.noreply.github.com>
2023-06-18 01:48:15 +0800

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,64 +1,48 @@
 import streamlit as st
 from textblob import TextBlob
-from PIL import Image
-import requests
-from io import BytesIO
 
-# Main App
-def main():
-    # Page configuration
-    st.set_page_config(page_title="Tourist Arrivals Dashboard", page_icon=":bar_chart:")
-
-    # Displaying chart for each country in separate tabs
-    st.title("Tourist Arrivals Dashboard")
-    st.write("Explore the tourist arrivals data for different countries.")
-
-    tabs = ["Indonesia", "Malaysia", "Singapore", "Thailand", "User Feedback"]
-    selected_tab = st.selectbox("Select a tab", tabs)
-
-    if selected_tab == "Indonesia":
-        display_chart("Indonesia")
-    elif selected_tab == "Malaysia":
-        display_chart("Malaysia")
-    elif selected_tab == "Singapore":
-        display_chart("Singapore")
-    elif selected_tab == "Thailand":
-        display_chart("Thailand")
-    elif selected_tab == "User Feedback":
-        display_user_feedback()
-
-
-# Function to display chart
-def display_chart(country_name):
-    st.write(f"## {country_name}")
-    placeholder_image = f"{country_name.lower()}_chart.png"
-    st.image(placeholder_image, use_column_width=True)
+def display_chart(country_name, iframe_code):
+    st.title(f"Tourist Arrivals in {country_name}")
+    st.write(f"Welcome to the Tourist Arrivals Dashboard for {country_name}. This dashboard provides an interactive visualization of the tourist arrivals data for {country_name}. Explore the chart below to understand the trends and patterns in tourist arrivals over time. The data is sourced from tradingeconomics.com.")
+    st.write(iframe_code, unsafe_allow_html=True)
     st.markdown(f"Source: [tradingeconomics.com](https://tradingeconomics.com/{country_name.lower().replace(' ', '-')}/tourist-arrivals)")
 
-# Function to get the chart image URL for each country
-def get_chart_image_url(country_name):
-    if country_name == "Indonesia":
-        return "https://d3fy651gv2fhd3.cloudfront.net/embed/?s=indonesiatouarr&v=202306050459V20230410&h=300&w=600"
-    elif country_name == "Malaysia":
-        return "https://d3fy651gv2fhd3.cloudfront.net/embed/?s=malaysiatouarr&v=202304061134V20230410&h=300&w=600"
-    elif country_name == "Singapore":
-        return "https://tradingeconomics.com/embed/?s=singaporetouarr&v=202306090122v20230410&h=300&w=600&ref=/singapore/tourist-arrivals"
-    elif country_name == "Thailand":
-        return "https://tradingeconomics.com/embed/?s=thailandtouarr&v=202305260335v20230410&h=300&w=600&ref=/thailand/tourist-arrivals"
+# Displaying chart for each country in separate tabs
+st.title("Tourist Arrivals Dashboard")
+st.write("Explore the tourist arrivals data for different countries.")
 
-# Function to load image from URL
-def load_image(image_url):
-    response = requests.get(image_url)
-    image = Image.open(BytesIO(response.content))
-    return image
+tabs = ["Indonesia", "Malaysia", "Singapore", "Thailand", "User Feedback"]
+selected_tab = st.selectbox("Select a tab", tabs)
 
-# Function to display user feedback
-def display_user_feedback():
+if selected_tab == "Indonesia":
+    st.write("## Indonesia")
+    indonesia_iframe = """
+    <iframe src='https://d3fy651gv2fhd3.cloudfront.net/embed/?s=indonesiatouarr&v=202306050459V20230410&h=300&w=600' height='300' width='600' frameborder='0' scrolling='no'></iframe>
+    """
+    display_chart("Indonesia", indonesia_iframe)
+elif selected_tab == "Malaysia":
+    st.write("## Malaysia")
+    malaysia_iframe = """
+    <iframe src='https://d3fy651gv2fhd3.cloudfront.net/embed/?s=malaysiatouarr&v=202304061134V20230410&h=300&w=600' height='300' width='600' frameborder='0' scrolling='no'></iframe>
+    """
+    display_chart("Malaysia", malaysia_iframe)
+elif selected_tab == "Singapore":
+    st.write("## Singapore")
+    singapore_iframe = """
+    <iframe src='https://tradingeconomics.com/embed/?s=singaporetouarr&v=202306090122v20230410&h=300&w=600&ref=/singapore/tourist-arrivals' height='300' width='600' frameborder='0' scrolling='no'></iframe>
+    """
+    display_chart("Singapore", singapore_iframe)
+elif selected_tab == "Thailand":
+    st.write("## Thailand")
+    thailand_iframe = """
+    <iframe src='https://tradingeconomics.com/embed/?s=thailandtouarr&v=202305260335v20230410&h=300&w=600&ref=/thailand/tourist-arrivals' height='300' width='600' frameborder='0' scrolling='no'></iframe>
+    """
+    display_chart("Thailand", thailand_iframe)
+elif selected_tab == "User Feedback":
     st.write("## User Feedback")
     st.write("Have something to say about the Tourist Arrivals Dashboard? Share your feedback with us!")
-    st.write("Enter your feedback in the text box below and click 'Submit'.")
-
-    feedback = st.text_input("Enter your feedback on the helpfulness of this website:")
+    st.write("Enter your feedback in the sidebar on the left and click 'Submit'.")
+   feedback = st.text_input("Enter your feedback on the helpfulness of this website:")
     if st.button("Submit"):
         if feedback:
             blob = TextBlob(feedback)
@@ -71,6 +55,6 @@ def display_user_feedback():
         else:
             st.warning("Please enter your feedback before submitting.")
 
-
 if __name__ == "__main__":
     main()
+
